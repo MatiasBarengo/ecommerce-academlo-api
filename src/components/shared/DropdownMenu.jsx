5cec3af1d74473589b9de7263e5266b65a5ca0c9
Matias Barengo <matiasbarengo99@gmail.com>
2023-05-11 18:02:02 -0300

    menu

--- src/components/shared/DropdownMenu.jsx
+++ src/components/shared/DropdownMenu.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/dropdownMenu.css'
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const DropdownMenu = () =>
 {
+
+  const [ menu, setMenu ] = useState( false )
+
+  const { user } = useSelector( state => state )
+
+  const handleMenu = () =>
+  {
+    setMenu( !menu )
+  }
+
   return (
     <div className='dropdown-container'>
-      <i className="fa-solid fa-bars"></i>
+      <i onClick={ handleMenu } className="fa-solid fa-bars"></i>
+      {
+        menu ? (
+          <div className='menu-container'>
+            <Link onClick={ handleMenu } className='menu-link' to={ '/login' }>
+              <i className="fa-solid fa-right-to-bracket"></i>
+              <span>Login</span>
+            </Link>
+            <Link to={ user ? '/profile' : '/login' } onClick={ handleMenu } className='menu-link'>
+              <i className="fa-solid fa-user"></i>
+              <span>Profile</span>
+            </Link>
+            <Link to={ user ? '/purchases' : '/login' } onClick={ handleMenu } className='menu-link'>
+              <i className="fa-solid fa-bag-shopping"></i>
+              <span>My Purchases</span>
+            </Link>
+            <Link to={ user ? '/cart' : 'login' } onClick={ handleMenu } className='menu-link'>
+              <i className="fa-solid fa-cart-shopping"></i>
+              <span>My Cart</span>
+            </Link>
+          </div>
+        ) : ( '' )
+      }
+
     </div>
   );
 };
